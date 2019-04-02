c5ff3ad898e62fe01db4c15a9d80acb3a66a6624
supergrecko <business@supergrecko.com>
2019-04-02 22:57:20 +0200

    New rate limiter infrastructure

--- src/RiotQuest/Components/Framework/RateLimit/Manager.php
+++ src/RiotQuest/Components/Framework/RateLimit/Manager.php
@@ -2,32 +2,48 @@
 
 namespace RiotQuest\Components\Framework\RateLimit;
 
+use Psr\SimpleCache\CacheInterface;
 use RiotQuest\Components\Framework\Client\Client;
-use RiotQuest\Components\Framework\Engine\Filesystem;
 
 class Manager
 {
 
     private $limits = [];
 
+    /**
+     * Cache namespace for requests
+     *
+     * @var CacheInterface
+     */
+    private $cache;
+
+    /**
+     * Manager constructor.
+     */
     public function __construct()
     {
-
+        $this->cache = Client::getCache('limits');
     }
 
-    public function __destruct()
+    /**
+     * Registers an API call to the cache
+     *
+     * @param $region
+     * @param string $endpoint
+     * @param string $key
+     * @param array $limits
+     * @throws \Psr\SimpleCache\InvalidArgumentException
+     */
+    public function registerCall($region, $endpoint = 'default', $key = 'standard', $limits = [1, 5])
     {
+        $ref = implode('.', [$key, $region, $endpoint]);
+        $now = json_decode($this->cache->get($ref), 1);
 
-    }
-
-    public function registerCall($region, $endpoint = 'default', $key = 'standard', $limits = [20, 60])
-    {
-
-    }
-
-    public function getLimits()
-    {
-        return $this->limits;
+        $this->cache->set($ref, json_encode([
+            'max' => $now['max'] ?? $limits[0],
+            'count' => (isset($now['count']) ? ($now['count'] + 1) : 0),
+            'ttl' => $now['ttl'] ?? (float)time() + $limits[1]
+        ]), $now['ttl'] ? $now['ttl'] - time() : $limits[1]);
     }
 
 }
