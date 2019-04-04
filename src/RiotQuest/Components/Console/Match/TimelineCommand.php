ff07af4ab1ec9157f7e0c2b26cd1b0269954d5d8
supergrecko <business@supergrecko.com>
2019-04-04 20:54:05 +0200

    CLI endpoints completed

---
+++ src/RiotQuest/Components/Console/Match/TimelineCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RiotQuest\Components\Console\Match;
+
+use RiotQuest\Components\Framework\Client\Client;
+use RiotQuest\Contracts\RiotQuestException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TimelineCommand extends Command
+{
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Match::timeline()')
+            ->setHelp('Match::timeline() function in the CLI')
+            ->setName('match:timeline')
+            ->addArgument('region', InputArgument::REQUIRED, 'Region')
+            ->addArgument('id', InputArgument::REQUIRED, 'Match ID');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (RIOTQUEST_ENVLOAD) {
+            $out = Client::match($input->getArgument('region'))->timeline($input->getArgument('id'));
+            $output->write(json_encode($out));
+        } else {
+            throw new RiotQuestException("App needs to be loaded using Environment to use RiotQuest from CLI.");
+        }
+    }
+
+}
