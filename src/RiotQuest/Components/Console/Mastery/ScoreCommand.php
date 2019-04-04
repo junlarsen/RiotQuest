f3b34485dcd538bea3b3a202c4239e4560536738
supergrecko <business@supergrecko.com>
2019-04-04 16:39:31 +0200

    Added more CLI commands for endpoints

---
+++ src/RiotQuest/Components/Console/Mastery/ScoreCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RiotQuest\Components\Console\Mastery;
+
+use RiotQuest\Components\Framework\Client\Client;
+use RiotQuest\Contracts\RiotQuestException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ScoreCommand extends Command
+{
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Mastery::score()')
+            ->setHelp('Mastery::score() function in the CLI')
+            ->setName('mastery:score')
+            ->addArgument('region', InputArgument::REQUIRED, 'Region')
+            ->addArgument('id', InputArgument::REQUIRED, 'Summoner ID');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (RIOTQUEST_ENVLOAD) {
+            $out = Client::mastery($input->getArgument('region'))->score($input->getArgument('id'));
+            $output->write(json_encode($out));
+        } else {
+            throw new RiotQuestException("App needs to be loaded using Environment to use RiotQuest from CLI.");
+        }
+    }
+
+}
