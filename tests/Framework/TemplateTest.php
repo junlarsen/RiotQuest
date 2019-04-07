9250f3bbd76fa40e8e8c64cb4f77a755b695f46b
supergrecko <business@supergrecko.com>
2019-04-07 22:38:03 +0200

    File system unit tests

---
+++ tests/Framework/TemplateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RiotQuest\Tests\Framework;
+
+use PHPUnit\Framework\TestCase;
+use RiotQuest\Components\Framework\Engine\Filesystem;
+
+class TemplateTest extends TestCase
+{
+
+    /**
+     * Test that the collection template works
+     *
+     * @throws \ReflectionException
+     */
+    public function testTemplateGeneration()
+    {
+        (new Filesystem())->generateTemplates();
+        $this->assertFileExists(__DIR__ . '/../../src/storage/templates/manifest.json');
+    }
+
+    /**
+     * Tests that wiping all the templates works
+     */
+    public function testTemplateFlush()
+    {
+        (new Filesystem())->flushTemplates();
+        $this->assertFileNotExists(__DIR__ . '/../../src/storage/templates/manifest.json');
+    }
+
+}
