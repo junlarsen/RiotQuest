ca9481a130a26fd780dced2604d9ea6323d0d88a
supergrecko <business@supergrecko.com>
2019-04-22 22:01:30 +0200

    Cache and CLI fix

--- src/RiotQuest/Components/Framework/Cache/CacheModel.php
+++ src/RiotQuest/Components/Framework/Cache/CacheModel.php
@@ -73,6 +73,7 @@ class CacheModel implements CacheInterface
     {
         if (is_string($ttl)) $ttl = explode(',', $ttl)[0];
         $key = $this->key($key);
+        @mkdir($this->path . $key, 755, true);
         file_put_contents($this->path . $key, json_encode([
             'data' => (string) $value,
             'ttl' => $ttl === null ? null : (float) time() + $ttl
