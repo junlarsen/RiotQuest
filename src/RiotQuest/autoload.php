203967abdb85445acae9402208fc653c796b4e93
supergrecko <business@supergrecko.com>
2019-04-25 16:03:47 +0200

    Removes the CLI due to bugs

--- src/RiotQuest/autoload.php
+++ src/RiotQuest/autoload.php
@@ -1,3 +1,7 @@
 <?php
 
 if (!defined('RIOTQUEST_ENV')) define('RIOTQUEST_ENV', 'API');
+
+if (!file_exists(__DIR__ . '/../storage/templates/manifest.json')) {
+    (new \RiotQuest\Components\Framework\Engine\Filesystem())->generateTemplates();
+}
